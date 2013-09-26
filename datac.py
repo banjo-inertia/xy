8794741884baea32c2be0975d9c07b6ed3fa7a75
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2013-09-25 22:59:04 -0400

    Switched from optparse to argparse.

--- datac.py
+++ datac.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Perform an action on a 2D set of data.
@@ -5,7 +6,7 @@ Perform an action on a 2D set of data.
 
 import os
 import logging
-import optparse
+import argparse
 
 
 def main():
@@ -13,33 +14,33 @@ def main():
     Method that always gets executed when program is run.
     """
 
-    usage = "usage: %prog [options] remotes_dir"
-    parser = optparse.OptionParser()
+    parser = argparse.ArgumentParser(description='Perform an action on a 2D set of data.')
 
-    parser.add_option("--clobber",
-                      help="Recalculate then mercilessly overwrite existing data.")
+    parser.add_argument("-c","--clobber",
+                      help="Recalculate then mercilessly overwrite existing data.",
+                      action="store_true")
 
-    parser.add_option("--data-dir",
-                      help="Directory in which to look for existing data/write new data [./].")
+    parser.add_argument("--data-dir",
+                      help="Directory in which to look for existing data/write new data (default: current directory).")
 
-    parser.add_option("--nodisplay",
-                      help="Don't display the plot onscreen before writing it.")
+    parser.add_argument("-n","--nodisplay",
+                      help="Don't display the plot on-screen before writing it.",
+                      action="store_true")
 
-    parser.add_option("--plot-dir",
-                      help="Directory in which to write plot image file [./].")
+    parser.add_argument("--plot-dir",
+                      help="Directory in which to write plot image file (default: current directory).")
 
-    parser.add_option("--plot-type",
-                      help="Extension indicating the type of image file to be written [eps].")
+    parser.add_argument("-t","--plot-type",
+                      help="Extension indicating the type of image file to be written (default: eps).",
+                      default="eps")
 
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
+
 
     # I first need to see if any data exists.
 
     # Next, I should load that data and check it to see if it matches the abscissa data I'm using to calculate the output I want. If it doesn't pass the check, I should throw an error and exit.
 
-
-    # if options.remotes_dir:
-    #     remotes = git_remotes_list(args[0])
 
 def get_program_filename():
     """
@@ -52,3 +53,6 @@ def get_name_root(name, prefix="plot", suffix=None):
     Strip off prefix and suffix from the specified name.
     """
     pass
+
+if __name__ == "__main__":
+    main()
