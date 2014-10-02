2a494efd72d34ac638763d162559d43fe3705698
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-10-02 16:23:43 -0400

    Add dummy class to test generate_ordinates

--- test/test_datac.py
+++ test/test_datac.py
@@ -2,14 +2,24 @@
 import datac
 import numpy as np
 import os
+import unittest
+
+class dummyclass(object):
+    """
+    Simple class for testing `generate_ordinates`
+    """
+    def __init__(self, params):
+        pass
+
+    def fun(self):
+        """
+        Return value of `True`
+        """
+        return True
+
 
 params = {"temp_sun": 6000.}
 bandgaps = np.linspace(0, 3.25, 100)
 
 abscissae = datac.generate_abscissae(bandgaps, "bandgap", params)
-pwd = os.getcwd()
-testdir = "test"
-filename = "data.dat"
-
-fqpn = os.path.join(pwd, testdir, filename)
-datac.write_json(fqpn, abscissae)
+data = datac.generate_ordinates(abscissae, dummyclass, "fun")
