89282c7bb1d1f7ae400f18e49d1ea07b8d2182f8
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-10-02 11:48:58 -0400

    Add distutils setup file
    Closes #2.

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name = "datac",
+      version = datac.__version__,
+      author = "Joshua Ryan Smith",
+      author_email = "joshua.r.smith@gmail.com",
+      packages = ["datac"],
+      url = "https://github.com/jrsmith3/datac",
+      description = "Utility for managing 2D data",
+      classifiers = ["Programming Language :: Python",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Development Status :: 3 - Alpha",
+                     "Intended Audience :: Science/Research",
+                     "Topic :: Utilities",
+                     "Natural Language :: English",],
+      install_requires = [],)
