3c510d53eccaebcd256de4db8242212f0bb069de
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-10-03 14:58:35 -0400

    Add some API tests
    These tests test if params, abscissae, and abscissa_name are
    read-only.

--- test/test_Datac.py
+++ test/test_Datac.py
@@ -45,3 +45,49 @@ class Instantiation(unittest.TestCase):
         abscissa_name = None
 
         self.assertRaises(TypeError, datac.Datac, params, abscissae, abscissa_name)
+
+
+class API(unittest.TestCase):
+    """
+    Test API behaves
+    """
+    def setUp(self):
+        """
+        Create a Datac object for testing
+        """
+        params = {"fake": 1.}
+        abscissae = [1, 2]
+        abscissa_name = "abscissa"
+
+        self.test_obj = datac.Datac(params, abscissae, abscissa_name)
+
+    def tearDown(self):
+        """
+        Destroy the test Datac object
+        """
+        del self.test_obj
+
+    def test_params_read_only(self):
+        """
+        The params attribute should be read-only
+        """
+        legit_params = {"one": 1.}
+
+        self.assertRaises(AttributeError, setattr, self.test_obj, "params", legit_params)
+
+    def test_abscissae_read_only(self):
+        """
+        The abscissae attribute should be read-only
+        """
+        legit_abscissae = [3, 4]
+
+        self.assertRaises(AttributeError, setattr, self.test_obj, "abscissae", legit_abscissae)
+
+    def test_abscissa_name_read_only(self):
+        """
+        The abscissa_name attribute should be read-only
+        """
+        legit_abscissa_name = "legit"
+
+        self.assertRaises(AttributeError, setattr, self.test_obj, "abscissa_name", legit_abscissa_name)
+
