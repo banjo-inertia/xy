6d7be493ab16215df9fca49662e9c40191d9daa6
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2015-04-10 09:59:26 -0400

    Clean up class documentation
    It was describing the old API and was therefore misleading.

--- datac/datac.py
+++ datac/datac.py
@@ -6,13 +6,6 @@ import matplotlib.pyplot as plt
 class Datac(collections.Sequence):
     """
     Model of 2D data with static parameters
-
-    A `Datac` object is instantiated with the following parameters:
-
-    :param dict params: Static parameters required to initialize the object featuring the ordinate calculator method.
-    :param list abscissae: Independent variable also required to initialize object featuring the ordinate calculator method.
-    :param str abscissa_name: Dictionary key for the abscissa name.
-    :param instancemethod calc_method: Instance method of a class which implements the calculator method used to determine the ordinates of the data.
     """
     # Many of the object's attributes should be read-only.
     @property
